a8c439c7b3b416c636f49e64e939855461c8873a
Canavoot Santhitiseree <canavoot.s@gmail.com>
2024-04-22 17:29:58 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://simple-book-api.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-      "/update": {
-        target: "https://simple-book-api.onrender.com",
-      },
-      "/remove": {
-        target: "https://simple-book-api.onrender.com",
-      },
-    },
-  },
->>>>>>> 2025e0cef77adc3ebc48ebe2a28fef7d40e023b8
   plugins: [react()],
 });
